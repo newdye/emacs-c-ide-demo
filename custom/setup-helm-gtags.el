890ce2b975935d9b1e4e3c555b36da4b0ddc6b80
newdye <hi.wszk@aliyun.com>
2016-11-24 16:39:25 +0800

    nothing change

--- custom/setup-helm-gtags.el
+++ custom/setup-helm-gtags.el
@@ -29,9 +29,9 @@
     (with-eval-after-load 'helm-gtags
       (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
       (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-      (define-key map (kbd "M-.") 'helm-gtags-dwim)
-      (define-key map (kbd "M-,") 'helm-gtags-pop-stack)
-      (define-key map (kbd "C-c <") 'helm-gtags-previous-history)
-      (define-key map (kbd "C-c >") 'helm-gtags-next-history))))
+      (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+      (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+      (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+      (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history))))
 
 (provide 'setup-helm-gtags)
